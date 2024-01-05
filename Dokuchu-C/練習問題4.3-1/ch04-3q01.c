089cb1b59e87f4b17c58a5a4ef66a63b0f4ae3dd
k2th0912 <k.kato@prosper-s.co.jp>
2024-01-05 10:05:26 +0900

    練習問題4.3-1の登録

---
+++ Dokuchu-C/練習問題4.3-1/ch04-3q01.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(int argc, char* argv[]) 
+{
+	/* コマンドライン引数が2個の場合、（プログラム名を含めてargcは3個）
+	 * そもそも問題文がおかしいような。。
+	 */
+	if (argc - 3) {
+		puts("not two!");
+	}
+	else {
+		puts("two!");
+	}
+
+}
